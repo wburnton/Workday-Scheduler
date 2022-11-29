f037135c97b99b92cd9535f328bd0efcf3339257
Will Burnton <willburnton@Wills-MacBook-Air.local>
2022-11-28 19:56:46 -0600

    readme url

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Workday-Scheduler
+# Workday-Scheduler 
+
+# Description 
+
+For this assignment, I have created a scheduling application that allows users to enter data into each hour of the day. Data entered is stored locally, and makes sure data remains on page even if it's refreshed. 
+
+Using dayjs(), the timeblock is color coded based on the current hour.
+
+
+
+# Usage 
+https://github.com/wburnton/Workday-Scheduler/  
+https://wburnton.github.io/Workday-Scheduler/
+
+![alt text](assets/screenshot.png)
+
+# License 
+
+This project used a MIT License 
--- script.js
+++ script.js
@@ -4,6 +4,8 @@
 var currentDay = dayjs().format('MMM D, YYYY, h:mm:ss a'); 
 $('#currentDay').text(currentDay); 
 var currentHour = dayjs().hour();
+//var currentHour = 10;
+console.log(currentHour);
 //timeId = ["hour8", "hour9", "hour10", "hour11", "hour12", "hour1", "hour2", "hour3", "hour4", "hour5"];
 // WHEN I scroll down
 // THEN I am presented with timeblocks for standard business hours 
@@ -12,14 +14,24 @@ var currentHour = dayjs().hour();
 // THEN each timeblock is color coded to indicate whether it is in the past, present, or future 
 function timeBlock () { 
     $(".time-block").each(function() {
-        var timeId = parseInt($(this).attr("id")); 
-
+      // timeId = the hour associated w/ the timeblock (number)
+        var timeBlocker = $(".time-block");
+        var timeId = parseInt($(this).attr("data-hour"));
+        console.log(timeId);
         if (currentHour > timeId) { 
-        timeId.addClass("future");
+        // add a class to the time-block div
+        timeBlocker.addClass("future");
+        timeBlocker.removeClass("past"); 
+        timeBlocker.removeClass("present");
         } else if (currentHour === timeId) { 
-        timeId.addClass("present");
+        timeBlocker.addClass("present");
+        timeBlocker.removeClass("future");
+        timeBlocker.removeClass("past");
+        
         } else { 
-        timeId.addClass("past");
+        timeBlocker.addClass("past");
+        timeBlocker.removeClass("future");
+        timeBlocker.removeClass("present");
         }; })
 
 }; 
@@ -42,11 +54,11 @@ $('.saveBtn').on('click', function () {
   $('#hour10 .description').val(localStorage.getItem('hour10'));
   $('#hour11 .description').val(localStorage.getItem('hour11'));
   $('#hour12 .description').val(localStorage.getItem('hour12'));
-  $('#hour1 .description').val(localStorage.getItem('hour1'));
-  $('#hour2 .description').val(localStorage.getItem('hour2'));
-  $('#hour3 .description').val(localStorage.getItem('hour3'));
-  $('#hour4 .description').val(localStorage.getItem('hour4'));
-  $('#hour5 .description').val(localStorage.getItem('hour5'));
+  $('#hour13 .description').val(localStorage.getItem('hour13'));
+  $('#hour14 .description').val(localStorage.getItem('hour14'));
+  $('#hour15 .description').val(localStorage.getItem('hour15'));
+  $('#hour16 .description').val(localStorage.getItem('hour16'));
+  $('#hour17 .description').val(localStorage.getItem('hour17'));
 // WHEN I refresh the page
 // THEN the saved events persist
 timeBlock();
